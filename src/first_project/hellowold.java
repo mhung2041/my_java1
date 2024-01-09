39344b37dc684a4fcb371670e13662d2dc3a7c35
Hung <mhung2041@gmail.com>
2024-01-10 02:20:13 +0700

    init project

---
+++ src/first_project/hellowold.java
@@ -0,0 +1,10 @@
+package first_project;
+
+public class hellowold {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
